4e0577643cc5a316841c27f1ac26fd792068e09e
John Won <johnwon.us@gmail.com>
2016-05-27 17:31:31 -0400

    [168. Excel Sheet Column Title][Time Limit Exceeded]committed by LeetCode Extension

--- 168-Excel-Sheet-Column-Title/solution.java
+++ 168-Excel-Sheet-Column-Title/solution.java
@@ -1,16 +1,6 @@
-public class Solution {
-    public String convertToTitle(int n) {
-        String solution = "";
-        double alphabet_count = (1/26);
-        int number_conversion;
-        while(n > 26)
-        {
-            number_conversion = (int) (Math.pow(n , alphabet_count));
-            solution += (char) (number_conversion + 64);
-            n -= Math.pow(26 , number_conversion);
-        }
-        solution += (char) (n + 64);
+class Solution {
+public:
+    string convertToTitle(int n) {
         
-        return solution;
     }
-}
+};
